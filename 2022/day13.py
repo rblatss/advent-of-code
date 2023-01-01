c6ed0756f49463844b339575062b77706325ae24
rblatner <rblatner@blueorigin.com>
2023-01-01 12:05:05 -0800

    i feel like the prompt for this one is ambiguous

---
+++ 2022/day13.py
@@ -0,0 +1,51 @@
+def compare(p1: list, p2: list) -> bool:
+
+    # If the right list runs out of items first, the lists are not in the correct order
+    if len(p1) > len(p2):
+        return False
+
+    # Compare items in lists
+    for i1, i2 in zip(p1, p2):
+        if isinstance(i1, list) and isinstance(i2, list):
+            if not compare(i1, i2):  # recurse
+                return False
+
+        # If exactly one value is an integer, compare integer
+        # to the first value in the list
+        elif isinstance(i1, list) and not isinstance(i2, list):
+            while isinstance(i1, list):
+                i1 = i1[0]
+            if i1 > i2:
+                return False
+        elif not isinstance(i1, list) and isinstance(i2, list):
+            while isinstance(i2, list):
+                i2 = i2[0]
+            if i1 > i2:
+                return False
+
+        # If both values are integers, the lower value should come first
+        else:
+            if i1 > i2:
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    indices = []
+    index = 1
+    with open("day13.txt", "r") as f:
+        while (p1 := f.readline()) and (p2 := f.readline()):
+            p1 = eval(p1.strip('\n'))
+            p2 = eval(p2.strip('\n'))
+
+            # Compare
+            if compare(p1, p2):
+                indices.append(index)
+
+            f.readline()  # skip empty line
+
+            index += 1
+
+    print(indices)
+    print(sum(indices))

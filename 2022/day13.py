c54501b5773d6bb8cc2aac609572648ddd7b5ccc
rblatner <rblatner@blueorigin.com>
2023-01-01 12:51:19 -0800

    ok, so the prompt was just difficult

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,40 +1,37 @@
-def find_integer(l: list) -> int:
-    for x in l:
-        if isinstance(x, list):
-            val = find_integer(x)
-            if val:
-                return val
-        else:
-            return x
-    return None
-
-
 def compare(p1: list, p2: list) -> bool:
-    # If the right list runs out of items first, the lists are not in the correct order
-    if len(p1) > len(p2):
-        return False
-
     # Compare items in lists
     for i1, i2 in zip(p1, p2):
         if isinstance(i1, list) and isinstance(i2, list):
-            if not compare(i1, i2):  # recurse
-                return False
+            r = compare(i1, i2)
+            if r is not None:  # recurse
+                return r
 
         # If exactly one value is an integer, compare integer
         # to the first value in the list
         elif isinstance(i1, list) and not isinstance(i2, list):
-            i1 = find_integer(i1)
-            if i1 and i1 > i2:
-                return False
-        elif not isinstance(i1, list) and isinstance(i2, list):
-            i2 = find_integer(i2)
-            if not i2 or i1 > i2:
-                return False
+            r = compare(i1, [i2])
+            if r is not None:
+                return r
 
-        # If both values are integers, the lower value should come first
+        elif not isinstance(i1, list) and isinstance(i2, list):
+            r = compare([i1], i2)
+            if r is not None:
+                return r
+
+        # If both values are integers, the lower value should come first.
+        # If the values are the same, continue checking <-- key!
         else:
             if i1 > i2:
                 return False
+            elif i1 < i2:
+                return True
+
+    # If the right list runs out of items first, the lists are not in the correct order
+    if len(p1) > len(p2):
+        return False
+
+    if len(p1) == len(p2):
+        return None
 
     return True
 
